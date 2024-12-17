505771a6a0e1ae5349480460bef7c67e483c6ea4
peter.pham31894@gmail.com <peter.pham31894@gmail.com>
2024-12-17 17:44:52 -0500

    ignore errors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Classes from './components/Classes/Classes';
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer'; // Assuming you have a Footer component
-import LandingBanner from './components/LandingBanner/LandingBanner';
-import HomeClass from './components/Classes/Classes';
-import Location from './components/Location/Location';
-import Overview from './components/Overview/Overview';
-import Social from './components/Social/Social';
-import Scheduler from './components/Schedule/Scheduler';
 import MuayThaiPage from './pages/Muay-Thai/muay-thai-page';
 import HomePage from './pages/Home/home-page';
 
