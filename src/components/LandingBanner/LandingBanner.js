e80d37b56d7e7888e057562f5c1850b43caad147
peter.pham31894@gmail.com <peter.pham31894@gmail.com>
2025-02-16 14:37:01 -0500

    Adding video banner for small screens

--- src/components/LandingBanner/LandingBanner.js
+++ src/components/LandingBanner/LandingBanner.js
@@ -11,12 +11,12 @@ const LandingBanner = () => {
       <iframe
         src={isMobile 
           ? "https://customer-2ubdanfjf4eg3lw0.cloudflarestream.com/e1ffca501313236c85ac69d6669589c5/iframe?muted=true&preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-2ubdanfjf4eg3lw0.cloudflarestream.com%2Fe1ffca501313236c85ac69d6669589c5%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600&controls=false"
-          : "https://customer-2ubdanfjf4eg3lw0.cloudflarestream.com/4716b8a1ad09e71758137fc1fc84e2b7/iframe?muted=true&preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-2ubdanfjf4eg3lw0.cloudflarestream.com%2F4716b8a1ad09e71758137fc1fc84e2b7%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600&controls=false"}
+          : "https://customer-2ubdanfjf4eg3lw0.cloudflarestream.com/603e6ad44c56676adc6396a6be9715b7/iframe?loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-2ubdanfjf4eg3lw0.cloudflarestream.com%2F603e6ad44c56676adc6396a6be9715b7%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600&controls=false"}
         loading="lazy"
         style={{ border: 'none', position: 'absolute', top: 0, left: 0, height: '100%', width: '100%' }}
         allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
         allowFullScreen
-        title="Cloudflare Stream Video"
+        muted={isMobile} // Ensure the video is muted on mobile devices
       ></iframe>
     </div>
   );
